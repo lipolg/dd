03461d10418599dd9fcf8e5dd085237895a6f07f
lipo <lipolg@163.com>
2020-07-20 23:13:30 +0800

    1、更新代码2

--- 2.py
+++ 2.py
@@ -4,18 +4,23 @@ import os
 
 
 def cfile(ldir):
-    farr = ldir.split("\\")
     num = 1
-    print(ldir)
+    s = 1
+    farr = ldir.split("\\") #获取文件名
     if os.path.exists(ldir):
         for f in os.listdir(ldir):
-            arr = f.split('.')
-            os.rename(os.path.join(ldir, f), os.path.join(ldir, farr[-1] + f'{num}.' + arr[1]))
-            num += 1
+            if os.path.isdir(os.path.join(ldir, f)):
+                fname = farr[-1] + f'第{s}季'
+                os.rename(os.path.join(ldir, f), os.path.join(ldir, fname)) #重命名文件夹
+                cfile(os.path.join(ldir, fname))
+                s += 1
+            else:
+                arr = f.split('.')  #取后缀
+                os.rename(os.path.join(ldir, f), os.path.join(ldir, farr[-1] + f'第{num}集.' + arr[1]))   #重命名文件
+                num += 1
     else:
         return '目录不存在！'
 
 
 if __name__ == '__main__':
-
-    cfile(r'D:\美剧\性教育')
+    cfile(r'D:\美剧\老爸老妈的浪漫史')
